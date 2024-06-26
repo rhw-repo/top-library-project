ae372f2178e4ce32cb6dfcce9c99e216f962cee7
Ruth Westnidge <rwestnidge@gmail.com>
2024-06-26 16:23:27 +0100

    Adds navbar with css styling & book JS constructor function. Plus TODO comments.

--- main.js
+++ main.js
@@ -5,10 +5,21 @@ take user’s input and store the new book objects into an array.
 https://www.theodinproject.com/lessons/node-path-javascript-library#project-solution
 */
 
+// Create an Array
 const myLibrary = [];
-
-function Book() {
-  // the constructor...
+ // the constructor...
+function Book(title, author, pages, read) {
+ 
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  // agregate and concatenate to make intelligble string
+  this.info = function info() {
+    return (
+      this.title + " by " + this.author + this.pages + " pages, " + this.read
+    );
+  };
 }
 
 function addBookToLibrary() {
