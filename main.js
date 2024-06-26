53501c3c177d221c79f5e32ff3bd432c9ff2e87a
Ruth Westnidge <rwestnidge@gmail.com>
2024-06-26 15:45:25 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,16 @@
+/* 
+All of your book objects are going to be stored in an array, 
+so add a function to the script (not the constructor) that can 
+take user’s input and store the new book objects into an array.  
+https://www.theodinproject.com/lessons/node-path-javascript-library#project-solution
+*/
+
+const myLibrary = [];
+
+function Book() {
+  // the constructor...
+}
+
+function addBookToLibrary() {
+  // add code here
+}
