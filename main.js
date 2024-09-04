72121b5d9f7248ad502b602944ce942cf931d90e
Ruth Westnidge <rwestnidge@gmail.com>
2024-09-04 14:51:34 +0100

    WIP Removes redundant tests and comments prior to modifying comments to use JSDoc

--- main.js
+++ main.js
@@ -1,40 +1,3 @@
-/* INSTRUCTIONS
-All of your book objects are going to be stored in an array, 
-so add a function to the script (not the constructor) that can 
-take user’s input and store the new book objects into an array.  
-https://www.theodinproject.com/lessons/node-path-javascript-library#project-solution
-*/
-
-
-
-
-// Log sanitized inputs to console for verification
-
-// THEN pass the sanitised inputs to the addBookToLibrary function
-
-
-// Test if main.js and if DOMPurify are loaded correctly
-// Add this at the very beginning of main.js
-//console.log('main.js is loaded and running');
-
-
-/*try {
-  // Test if DOMPurify is loaded correctly
-  if (typeof DOMPurify === 'undefined') {
-      console.error('DOMPurify is not loaded or not defined.');
-  } else {
-      console.log('DOMPurify is loaded correctly.');
-  }
-
- // Test code to confirm DOMPurify is working
-const testInput = '<img src="x" onerror="alert(\'test\')">This is a test</img>';
-console.log('Original Input:', testInput); // Log the raw, potentially unsafe input
-
-const cleanOutput = DOMPurify.sanitize(testInput);
-console.log('Sanitized Output:', cleanOutput); // This will show the sanitized version, without harmful attributes*/
-
-
-// Create an Array
 const myLibrary = [];
 
 // Constructor
@@ -46,13 +9,16 @@ function Book(title, author, pages, read) {
   console.log(this);
 }
 
-// Example book to check Book function runs and show book format to user
-const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, "Not read yet");
-myLibrary.push(theHobbit);
+// Test Book function and display "book format" to user
+const sample = new Book(
+  "Sample Book", "Sample Author", 295, "Not read yet"
+);
+myLibrary.push(sample);
 console.log(myLibrary);
 displayBooks();
 
-// Push a new book to array, is later called with form input data passed to it
+// Push new book to array. 
+// Called when sanitized form input data passed to it.
 function addBookToLibrary(title, author, pages, read) {
   // Create a new Book object
   const newBook = new Book(title, author, pages, read);
@@ -139,7 +105,7 @@ function displayBooks() {
 
 // Select the form, add Event Listener to form's submit event
 const addForm = document.getElementById("add-book");
-console.log("Form selected:", addForm); 
+console.log("Form selected:", addForm);
 addForm.addEventListener("submit", function (e) {
   // Prevent default submission as functions should run instead
   e.preventDefault();
@@ -149,25 +115,13 @@ addForm.addEventListener("submit", function (e) {
   const pages = this.querySelector('input[name="pages"]').value;
   const read = this.querySelector('input[id="switch"]').checked;
 
- 
-  /* Log the raw inputs to console for comparison
-  console.log("Raw Form data:", { title, author, pages, read });*/
+  // Sanitize inputs
+  const sanitizedTitle = DOMPurify.sanitize(title);
+  const sanitizedAuthor = DOMPurify.sanitize(author);
+  const sanitizedPages = DOMPurify.sanitize(pages);
 
-   // Sanitize inputs using DOMPurify
-   const sanitizedTitle = DOMPurify.sanitize(title);
-   const sanitizedAuthor = DOMPurify.sanitize(author);
-   const sanitizedPages = DOMPurify.sanitize(pages);
- 
-   /* Log sanitized inputs to console for verification
-   console.log("Sanitized Form data:", {
-     title: sanitizedTitle,
-     author: sanitizedAuthor,
-     pages: sanitizedPages,
-     read,
-   });*/
-
-// Call addBookToLibrary passing in sanitized form data
-addBookToLibrary(sanitizedTitle, sanitizedAuthor, sanitizedPages, read);
+  // Call addBookToLibrary passing in sanitized form data
+  addBookToLibrary(sanitizedTitle, sanitizedAuthor, sanitizedPages, read);
 
   // Clear form fields after submission
   this.reset();
@@ -192,7 +146,3 @@ document
     // call close() method
     dialog.close();
   });
-
-/*} catch (error) {
-  console.error('An error occurred:', error);
-}*/
